a18f25b315030209884d850dca69203dd059a354
Sandeep Vishwakarma <62471058+sv2441@users.noreply.github.com>
2023-09-28 18:02:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,4 @@ st.image('logo.png')
 st.title("👨‍💻 OP Generation")
 
 
-embedded_code = """
-<iframe src="https://www.canva.com/design/DAFvushARmE/watch" width="768" height="432" frameborder="0" allowfullscreen="true"></iframe>
-"""
-st.markdown(embedded_code, unsafe_allow_html=True)
 
