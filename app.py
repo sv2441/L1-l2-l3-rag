8817a52ee4a71a67de98db525aef399de13fcff5
Sandeep Vishwakarma <62471058+sv2441@users.noreply.github.com>
2023-09-28 15:55:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ st.image('logo.png')
 st.title("👨‍💻 OP Generation")
 
 embedded_code = """
-<iframe width="768" height="432" src="https://miro.com/app/embed/uXjVMnDynt4=/?pres=1&frameId=3458764565130553333&embedId=649838642305" frameborder="0" scrolling="no" allow="fullscreen; clipboard-read; clipboard-write" allowfullscreen></iframe>
+<iframe width="768" height="432" src="https://miro.com/app/embed/uXjVMnDynt4=/?pres=1&frameId=3458764565130553333&embedId=649838642305" frameborder="0" scrolling="yes" allow="fullscreen; clipboard-read; clipboard-write" allowfullscreen></iframe>
 """
 st.markdown(embedded_code, unsafe_allow_html=True)
