56461fd1f4d8fde595d328f61c1fae28a52d7899
Sandeep Vishwakarma <62471058+sv2441@users.noreply.github.com>
2023-09-28 15:42:01 +0530

    Update L1 OP Generation.py

--- pages/L1 OP Generation.py
+++ pages/L1 OP Generation.py
@@ -22,6 +22,7 @@ os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 # os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
     
 chat_llm = ChatOpenAI(temperature=0.6)
+# chat_llm = ChatOpenAI(temperature=0.6)
 
 
 def dict_to_csv(data, filename, append=False):
@@ -39,69 +40,66 @@ def dict_to_csv2(data, filename, append=False):
         if not append:
             writer.writeheader()
         writer.writerow({'L1 Intended Results': data})       
-
-
-def convert_dict_to_csv(data_dict):
-    with open('data11.csv', 'w', newline='') as csvfile:
-        fieldnames = ['Pre/Post', 'Activity']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        # Check if the file is empty and write the header only if it's empty
-        is_file_empty = csvfile.tell() == 0
-        if is_file_empty:
-            writer.writeheader()
-
-        for key, value in data_dict.items():
-            if isinstance(value, list):
-                for item in value:
-                    writer.writerow({'Pre/Post': key, 'Activity': item})
-            else:
-                writer.writerow({'Pre/Post': key, 'Activity': value})
-
+        
 def get_download_link(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="final.csv">Download CSV File</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="PA-results.csv">Download CSV File</a>'
     return href
 
+# def convert_dict_to_csv(data_dict):
+#     with open('data11.csv', 'w', newline='') as csvfile:
+#         fieldnames = ['Pre/Post', 'Activity']
+#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
+#         # Check if the file is empty and write the header only if it's empty
+#         is_file_empty = csvfile.tell() == 0
+#         if is_file_empty:
+#             writer.writeheader()
 
-def prompta_generator(df):
+#         for key, value in data_dict.items():
+#             if isinstance(value, list):
+#                 for item in value:
+#                     writer.writerow({'Pre/Post': key, 'Activity': item})
+#             else:
+#                 writer.writerow({'Pre/Post': key, 'Activity': value})
+
+# def prompta_generator(df):
     
-    #####output parser #############################################
+#     #####output parser #############################################
 
-    Action_schema = ResponseSchema(name="Actionable",
-                                description="List of Actionable requirements from the text")
+#     Action_schema = ResponseSchema(name="Actionable",
+#                                 description="List of Actionable requirements from the text")
 
-    response_schemas = [ Action_schema]
-    output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
-    format_instructions = output_parser.get_format_instructions()
+#     response_schemas = [ Action_schema]
+#     output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+#     format_instructions = output_parser.get_format_instructions()
 
-    ###########################################################################
+#     ###########################################################################
 
-    title_template = """ \ You are an AI Governance bot.
-                Extract actionable requirements from the "{topic}".
-               {format_instructions}
-                """
-    prompt = ChatPromptTemplate.from_template(template=title_template)
+#     title_template = """ \ You are an AI Governance bot.
+#                 Extract actionable requirements from the "{topic}".
+#                {format_instructions}
+#                 """
+#     prompt = ChatPromptTemplate.from_template(template=title_template)
 
-##############################################################################################
+# ##############################################################################################
     
-    for index, row in df.iterrows():
-        messages = prompt.format_messages(topic=row['Description'], format_instructions=format_instructions)
-        response = chat_llm(messages)
-        response_as_dict = output_parser.parse(response.content)
-        data = response_as_dict
-        convert_dict_to_csv(data)
-        test=pd.DataFrame(df.iloc[index]).T
-        data11 = pd.read_csv(r'data11.csv',encoding='cp1252')
-        results = pd.concat([test, data11], axis=1).fillna(0)
-        # result.to_csv('final1.csv')
-        # test=pd.DataFrame(df.iloc[0]).T
-        # results = pd.concat([test, data11], axis=1).fillna(0)
-        results.to_csv('PA-results.csv', mode='a', header=not os.path.isfile('PA-results.csv'), index=False)
-    st.subheader("OP's")
-    st.dataframe(results)
+#     for index, row in df.iterrows():
+#         messages = prompt.format_messages(topic=row['Description'], format_instructions=format_instructions)
+#         response = chat_llm(messages)
+#         response_as_dict = output_parser.parse(response.content)
+#         data = response_as_dict
+#         convert_dict_to_csv(data)
+#         test=pd.DataFrame(df.iloc[index]).T
+#         data11 = pd.read_csv(r'data11.csv',encoding='cp1252')
+#         results = pd.concat([test, data11], axis=1).fillna(0)
+#         # result.to_csv('final1.csv')
+#         # test=pd.DataFrame(df.iloc[0]).T
+#         # results = pd.concat([test, data11], axis=1).fillna(0)
+#         results.to_csv('PA-results.csv', mode='a', header=not os.path.isfile('PA-results.csv'), index=False)
+#     st.subheader("OP's")
+#     st.dataframe(results)
 
 
 def description_generator(df):
@@ -121,12 +119,13 @@ def description_generator(df):
             3) Provide a deeper understanding of what the activity entails
             4) Do not include expected outcome
             5) Do not include justification on why the activity should be implemented
+            Use words like establish, define, create, execute, perform, provide, validate, approve, review, monitor as action words.
             {format_instructions}
             """
     prompt = ChatPromptTemplate.from_template(template=title_template)
     
     for index, row in df.iterrows():
-        messages = prompt.format_messages(topic=row["Activity"], format_instructions=format_instructions)
+        messages = prompt.format_messages(topic=row["Actionables"], format_instructions=format_instructions)
         response = chat_llm(messages)
         response_as_dict = output_parser.parse(response.content)
         data = response_as_dict
@@ -148,6 +147,7 @@ def l1_title_generator(df):
 
     title_template = """ \ You are an AI Governance bot.
                 Summarize actionable in max of 10 words for "{topic}". Ensure that the output has an action word.
+                
                {format_instructions}
                 """
     prompt = ChatPromptTemplate.from_template(template=title_template)
@@ -163,7 +163,7 @@ def l1_title_generator(df):
     data13 = pd.read_csv(r'data13.csv',encoding='cp1252',names=['L1 title'])
     results = pd.concat([df, data13], axis=1)
     results.to_csv("PA-results.csv")
-    st.subheader("Title Result")
+    st.subheader("Description Result")
     st.dataframe(results)   
 
 def intended_results_generator(df):
@@ -236,6 +236,7 @@ def specifications_generator(df):
 
     title_template = """ \ You are an AI Governance bot.
                 List any specific conditions or considerations mentioned in the following "{topic}".
+                Do not generalize. Do not exhibit any prior knowledge other than content provided above. Mention "no" in case there are no conditions.
                 """
 
     prompt = ChatPromptTemplate.from_template(template=title_template)
@@ -250,7 +251,7 @@ def specifications_generator(df):
     result.to_csv('PA-results.csv', index=False)  
     st.subheader("Specifications")
     st.dataframe(result)
-    # final_result=pd.read_csv('PA-results.csv',usecols=['CO Level123 Code','Level','Code','Name','Description','Prompt A','L1 Description','L1 title','L1 Intended Results','L1 Artefact Description','L1 Specifications'])
+    # final_result=pd.read_csv('PA-results.csv',usecols=['CO Level123 Code','Level','Code','Name','Description','Prompt A','L1 Description','L1 title','L1 Intended Results',' L1 Artefact Description','L1 Specifications'])
     
     st.markdown(get_download_link(result), unsafe_allow_html=True)
     
@@ -265,18 +266,15 @@ def main():
 
     if file is not None:
         # Read CSV file
-        df = pd.read_csv(file)
+        L1_df = pd.read_csv(file)
 
         # Display preview
         st.subheader("CSV File Preview")
-        st.dataframe(df)
+        st.dataframe(L1_df)
 
-        # Button to process the file
-        if st.button("Extract Actionable"):
-            prompta_generator(df)
             
         if st.button("Generate Description"):
-            description_generator(pd.read_csv('PA-results.csv'))
+            description_generator(L1_df)
         
         if st.button("Generate Title"):
             l1_title_generator(pd.read_csv('PA-results.csv'))
