27c8ef638be5e43e1835421441a8ca581a57f316
Sandeep Vishwakarma <62471058+sv2441@users.noreply.github.com>
2023-09-28 16:24:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,4 @@ st.set_page_config(
 st.image('logo.png')
 st.title("👨‍💻 OP Generation")
 
-embedded_code = """
-<iframe src="https://www.canva.com/design/DAFvushARmE/watch" width="768" height="432" frameborder="0" allowfullscreen="true"></iframe>
-"""
-st.markdown(embedded_code, unsafe_allow_html=True)
+
