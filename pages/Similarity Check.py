1029b0d7f3572d390c7d678644045924a0f8a6e9
Sandeep Vishwakarma <62471058+sv2441@users.noreply.github.com>
2023-10-20 15:47:23 +0530

    Update Similarity Check.py

--- pages/Similarity Check.py
+++ pages/Similarity Check.py
@@ -1,64 +1,89 @@
-### Lib 
-from langchain.document_loaders import CSVLoader
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.chains import RetrievalQA
-from langchain.llms import OpenAI
 import streamlit as st
+import numpy as np
 import os
+import time
 import base64
+from datetime import datetime
+
+from sentence_transformers import SentenceTransformer, util
 import pandas as pd
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-# os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
-
-loader = CSVLoader(file_path='op_description_from_reg.csv',encoding="utf8")
-index_creator = VectorstoreIndexCreator()
-docsearch = index_creator.from_loaders([loader])
 
 
-chain = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.vectorstore.as_retriever(), input_key="question")
+corpus_df = pd.read_csv("registry_data.csv")
+
+model_path= "all-MiniLM-L6-v2"
+embedder = SentenceTransformer(model_path)
+
+# Encode the corpus
+corpus_embeddings = embedder.encode(corpus_df["OPs"], convert_to_tensor=True)
 
 
-
-def generator(df):
+def get_download_link(df):
+    current_datetime = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    file_name = f"similar_{current_datetime}.csv"
     
-    result_df = pd.DataFrame(columns=['L1 Title', 'Similar'])
-    for index ,row in df.iterrows():
-        query =f""" Provide the "OP Name" and its "New code" as OP reference of Concatenated Column.
-                if concatented text has same meaning as or has higher similarity ranking to "{row['L1 Title']}". 
-            
-            """
-        response = chain({"question": query})
-        result_df = result_df.append({'L1 Title': row['L1 Title'], 'Similar': response['result']}, ignore_index=True)
-    return result_df
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{file_name}">Download CSV File</a>'
     
+    return href
 
- 
-### main
+def similarity_finder(df):
+     
+    df2 = pd.DataFrame(columns=['Title','New Code', 'Similar', 'Similarity Score'])
+    op_to_new_code = dict(zip(corpus_df["OPs"], corpus_df["New Code"]))
+
+    similar_counts = {}
+
+    for index, row in df.iterrows():
+        query = row["Title"]
+        similarity_threshold = 0.6
+
+        query_embedding = embedder.encode(query, convert_to_tensor=True)
+        hits = util.semantic_search(query_embedding, corpus_embeddings)
+
+        hits = hits[0]
+        found_results = False
+        similar_count = 0
+
+        for hit in hits:
+            hit_id = hit['corpus_id']
+            title = corpus_df.iloc[hit_id]["OPs"]
+            score = hit['score']
+
+            if score > similarity_threshold and similar_count < 10:
+                
+                new_code = op_to_new_code.get(title, "N/A")
+                df2 = df2.append({'Title': query, 'New Code': new_code, 'Similar': title, 'Similarity Score': score}, ignore_index=True)
+                found_results = True
+                similar_count += 1
+    
+                if query in similar_counts:
+                    similar_counts[query] += 1
+                else:
+                    similar_counts[query] = 1
+        
+        if not found_results:
+            df2 = df2.append({'Title': query,'New Code': "Didn't Found",'Similar': "Didn't Found", 'Similarity Score': "Didn't Found"}, ignore_index=True)
+
+    # Save the results to a CSV file
+    # df2.to_csv("similarity_L1_results.csv", index=False)
+    st.subheader("Similarity")
+    st.dataframe(df2)
+    st.markdown(get_download_link(df2), unsafe_allow_html=True)
+    
 st.title("👨‍💻 Prodago RAG")
-
-
-
-# Streamlit UI
 st.write("OP Similarity Search")
 
 uploaded_file = st.file_uploader("Upload a file", type=["xlsx", "xls","csv"])
 
+st.markdown("### Download Sample CSV")
+sample = pd.read_csv("sample_similarity.csv")
+st.markdown(get_download_link(sample), unsafe_allow_html=True)
+
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file,encoding="latin-1")
-    st.write(df)
+    ldf = pd.read_csv(uploaded_file,encoding="latin-1")
+    st.write(ldf)
 
-    if st.button("Generate Similarities", key="generate"):
-        # data=pd.read_csv("fdata.csv")
-        result_df = generator(df)
-        st.write(result_df)
-
-        # Download button for the generated result
-        st.markdown('### Download Result')
-        result_csv = result_df.to_csv(index=False)
-        b64 = base64.b64encode(result_csv.encode()).decode()
-        href = f'<a href="data:file/csv;base64,{b64}" download="result.csv">Download CSV</a>'
-        st.markdown(href, unsafe_allow_html=True)
+    if st.button("Generate Similarities"):
+        similarity_finder(ldf)
