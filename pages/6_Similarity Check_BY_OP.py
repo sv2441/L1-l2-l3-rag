37d4bbe7bd2f86137017545d31a9fc9b7edfff99
Sandeep Vishwakarma <62471058+sv2441@users.noreply.github.com>
2023-10-20 15:44:41 +0530

    Update and rename Generate Actionables.py to 6_Similarity Check_BY_OP.py

---
+++ pages/6_Similarity Check_BY_OP.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import numpy as np
+import time
+import base64
+from datetime import datetime
+from sentence_transformers import SentenceTransformer, util
+import pandas as pd
+
+# sample_data_path = "sample_data"
+corpus_df = pd.read_csv("registry_data.csv")
+
+model_path= "all-MiniLM-L6-v2"
+embedder = SentenceTransformer(model_path)
+
+# Encode the corpus
+corpus_embeddings = embedder.encode(corpus_df["OPs"], convert_to_tensor=True)
+
+
+def get_download_link(df):
+    current_datetime = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    file_name = f"similar_{current_datetime}.csv"
+    
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{file_name}">Download CSV File</a>'
+    
+    return href
+
+def similarity_finder(Input):
+    
+    df2 = pd.DataFrame(columns=['Title','New Code', 'Similar', 'Similarity Score'])
+    op_to_new_code = dict(zip(corpus_df["OPs"], corpus_df["New Code"]))
+    similar_counts = {}
+
+    query = Input
+    similarity_threshold = 0.6
+
+    query_embedding = embedder.encode(query, convert_to_tensor=True)
+    hits = util.semantic_search(query_embedding, corpus_embeddings)
+
+    hits = hits[0]
+    found_results = False
+    similar_count = 0
+
+    for hit in hits:
+        hit_id = hit['corpus_id']
+        title = corpus_df.iloc[hit_id]["OPs"]
+        score = hit['score']
+
+        if score > similarity_threshold and similar_count < 10:
+            
+            new_code = op_to_new_code.get(title, "N/A")
+            df2 = df2.append({'Title': query, 'New Code': new_code, 'Similar': title, 'Similarity Score': score}, ignore_index=True)
+            found_results = True
+            similar_count += 1
+
+            if query in similar_counts:
+                similar_counts[query] += 1
+            else:
+                similar_counts[query] = 1
+    
+    if not found_results:
+        df2 = df2.append({'Title': query,'New Code': "Didn't Found",'Similar': "Didn't Found", 'Similarity Score': "Didn't Found"}, ignore_index=True)
+    # Save the results to a CSV file
+    # df2.to_csv("similarity_L1_results.csv", index=False)
+    st.subheader("Similarity")
+    st.dataframe(df2)
+    st.markdown(get_download_link(df2), unsafe_allow_html=True)
+    
+st.title("👨‍💻 Prodago RAG")
+st.write("OP Similarity Search")
+
+OP_title=st.text_input("Enter the OP Title")
+OP_description=st.text_input("Enter the OP Description")
+OP= OP_title + " " + OP_description
+
+if st.button("Generate Similarities"):
+        similarity_finder(OP)
